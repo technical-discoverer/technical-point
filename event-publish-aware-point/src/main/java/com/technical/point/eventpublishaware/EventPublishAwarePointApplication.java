903cb54076268c97eb26d19717b434de8c53245a
gaogba <gaogba@yonyou.com>
2020-08-28 18:03:07 +0800

    ApplicationEventPublisherAware事件 init project

---
+++ event-publish-aware-point/src/main/java/com/technical/point/eventpublishaware/EventPublishAwarePointApplication.java
@@ -0,0 +1,13 @@
+package com.technical.point.eventpublishaware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPublishAwarePointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPublishAwarePointApplication.class, args);
+    }
+
+}
---
+++ event-publish-aware-point/src/test/java/com/technical/point/eventpublishaware/EventPublishAwarePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical.point.eventpublishaware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPublishAwarePointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
