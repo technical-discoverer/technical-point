386384be3338db1dbea39b8964de7ac98752b726
gaogba <gaogba@yonyou.com>
2020-12-23 20:05:33 +0800

    练习HashMap

---
+++ technical-point-list/src/main/java/com/technical/point/list/test/TestHashMap.java
@@ -0,0 +1,20 @@
+package com.technical.point.list.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description 测试HashMap部分方法
+ * @Author gaogba
+ * @Date 2020/12/23 17:10
+ * @Version 1.0
+ */
+public class TestHashMap {
+    public static void main(String[] args) {
+
+    }
+
+    public static void test(){
+        Map<String,Object> params = new HashMap<>(4);
+    }
+}
